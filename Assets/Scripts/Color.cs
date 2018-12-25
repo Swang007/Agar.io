a803f1af6c0567242cd1bbd5870995c4a289f2f4
Sean Wangnick <Swang007@users.noreply.github.com>
2018-12-24 23:36:17 -0700

    Init Agar.io

---
+++ Assets/Scripts/Color.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Color : MonoBehaviour
+{
+    public List<Material> Mats = new List<Material>();
+
+    void Awake()
+    {
+        GetComponent<Renderer>().material = Mats[Random.Range(0, Mats.Count)];
+    }
+}
---
+++ Assets/Scripts/Eat.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Eat : MonoBehaviour
+{
+    public string Tag;
+    public Text Letters;
+    public float Increase;
+
+    int Score = 0;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == Tag)
+        {
+            transform.localScale += new Vector3(Increase, Increase, Increase);
+            Destroy(other.gameObject);
+
+            Score += 10;
+            Letters.text = "Score: " + Score;
+        }
+    }
+}
